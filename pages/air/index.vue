d36b74b8cfebf4ccf13f22e514ae079783ca4f4e
itcast-hsian <541510140@qq.com>
2019-10-11 14:44:28 +0800

    初始化

---
+++ pages/air/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>air</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>post</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
