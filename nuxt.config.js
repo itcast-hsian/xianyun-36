8be445ac578a0e91ab488f543e6c047203d227b8
itcast-hsian <541510140@qq.com>
2019-10-11 15:17:00 +0800

    tab栏切换

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,15 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "世界上最大的旅游网站",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'} // 新增全局字体样式
     ]
   },
   /*
@@ -23,7 +24,8 @@ export default {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    'assets/main.css' // 新增自定义的页面过渡样式
   ],
   /*
   ** Plugins to load before mounting the App
@@ -48,6 +50,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径 
   },
   /*
   ** Build configuration
---
+++ pages/tab.vue
@@ -0,0 +1,68 @@
+<template>
+    <div style="padding:50px;">
+      
+        <div class="btns">
+            <span v-for="(item, index) in ['新闻', '娱乐', '体育']"
+            :key="index"
+            @click="handleClick(index)"
+            :class="{ active: index === current }">
+                {{item}}
+            </span>
+        </div>
+      
+        <div v-if="current === 0">
+            <p>未来5年什么技能值钱？这样玩PS的人早加薪了</p>
+            <p>未来5年什么技能值钱？这样玩PS的人早加薪了</p>
+            <p>未来5年什么技能值钱？这样玩PS的人早加薪了</p>
+            <p>未来5年什么技能值钱？这样玩PS的人早加薪了</p>
+        </div>  
+
+        <div v-if="current === 1">
+            <p>AKIRA公开谈老婆林志玲 甜蜜放闪：当成花一样爱</p>
+            <p>AKIRA公开谈老婆林志玲 甜蜜放闪：当成花一样爱</p>
+            <p>AKIRA公开谈老婆林志玲 甜蜜放闪：当成花一样爱</p>
+            <p>AKIRA公开谈老婆林志玲 甜蜜放闪：当成花一样爱</p>
+        </div>  
+
+        <div  v-if="current === 2">
+            <p>杨旭:7-0为祖国70周年献礼 为国征战无限荣耀!</p>
+            <p>杨旭:7-0为祖国70周年献礼 为国征战无限荣耀!</p>
+            <p>杨旭:7-0为祖国70周年献礼 为国征战无限荣耀!</p>
+            <p>杨旭:7-0为祖国70周年献礼 为国征战无限荣耀!</p>
+        </div>  
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            // 一个框架的学习可以通过tab栏来总结，该例子最重要的核心就是当前的下标的变量
+            current: 0
+        }
+    },
+
+    methods: {
+        // 点击时候改变current
+        handleClick(index){
+            this.current = index;
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .btns{
+        margin-bottom: 20px;
+        span{
+            cursor: pointer;
+            border:1px #ddd solid;
+            padding: 10px;
+        }
+
+        .active{
+            color:red;
+            border-color: red;
+        }
+    }
+</style>
