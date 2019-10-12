a876afbea310446bb64829d49be2a6706cd171ea
itcast-hsian <541510140@qq.com>
2019-10-12 09:57:03 +0800

    新建登录表单

---
+++ components/user/loginForm.vue
@@ -0,0 +1,92 @@
+<template>
+    <el-form 
+        :model="form" 
+        ref="form"
+        :rules="rules" 
+        class="form">
+
+        <el-form-item class="form-item" prop="username">
+            <el-input 
+            v-model="form.username"
+            placeholder="用户名/手机">
+            </el-input>
+        </el-form-item>
+
+        <el-form-item class="form-item" prop="password">
+            <el-input 
+            v-model="form.password"
+            placeholder="密码" 
+            type="password">
+            </el-input>
+        </el-form-item>
+
+        <p class="form-text">
+            <nuxt-link to="#">忘记密码</nuxt-link>
+        </p>
+
+        <el-button 
+        class="submit"
+        type="primary"
+        @click="handleLoginSubmit">
+            登录
+        </el-button>
+    </el-form>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            // 表单数据
+            form: {
+                username: "",
+                password: ""
+            },
+            // 表单规则
+            rules: {
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                ]
+            },
+        }
+    },
+    methods: {
+        // 提交登录
+        handleLoginSubmit(){
+           this.$refs.form.validate(valid => {
+               // valid是表单验证的结果
+               if(valid){
+                   // 提交登录接口
+                   
+               }
+           })
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .form{
+        padding:25px;
+    }
+
+    .form-item{
+        margin-bottom:20px;
+    }
+
+    .form-text{
+        font-size:12px;
+        color:#409EFF;
+        text-align: right;
+        line-height: 1;
+    }
+
+    .submit{
+        width:100%;
+        margin-top:10px;
+    }
+</style>
--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,13 +1,96 @@
 <template>
-  <div>login</div>
+    <div class="container">
+        <!-- 主要内容 -->
+        <el-row 
+        type="flex" 
+        justify="center" 
+        align="middle" 
+        class="main">
+
+            <div class="form-wrapper">
+                <!-- 表单头部tab -->
+                <el-row type="flex" justify="center" class="tabs">
+
+                    <span 
+                    :class="{active: currentTab === index}" 
+                    v-for="(item, index) in [`登录`, `注册`]"
+                    :key="index" 
+                    @click="handleChangeTab(index)">
+                        {{item}}
+                    </span>
+                </el-row>
+
+                <!-- 登录功能组件 -->
+                <LoginForm v-if="currentTab == 0"/>
+
+                <!-- 注册功能组件 -->
+                <!-- <RegisterForm v-if="currentTab == 1"/> -->
+            </div>
+        </el-row>
+    </div>
 </template>
 
 <script>
-export default {
+import LoginForm from "@/components/user/loginForm"
 
+export default {
+    data(){
+        return {
+            currentTab: 0
+        }
+    },
+    components: {
+      LoginForm
+    },
+    methods: {
+        handleChangeTab(index){
+            this.currentTab = index;
+        },
+    }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container{
+    background:url(http://157.122.54.189:9095/assets/images/th03.jfif) center 0;
+    height: 700px;
+    min-width:1000px;
 
+    .main{
+        width:1000px;
+        height: 100%;
+        margin:0 auto;
+        position: relative;
+        
+        .form-wrapper{
+            width:400px;
+            margin:0 auto;
+            background:#fff;
+            box-shadow: 2px 2px 0 rgba(0,0,0,0.1);
+            overflow:hidden;
+            
+            .tabs{
+                span{
+                    display: block;
+                    width:50%;
+                    height: 50px;
+                    box-sizing: border-box;
+                    border-top:2px #eee solid;
+                    background:#eee;
+                    line-height: 48px;
+                    text-align: center;
+                    cursor: pointer;
+                    color:#666;
+
+                    &.active{
+                        color:orange;
+                        border-top-color: orange;
+                        background:#fff;
+                        font-weight: bold;
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
