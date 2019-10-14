8e46fd12d3fcfafca78bf4b1030a34bd75568e03
itcast-hsian <541510140@qq.com>
2019-10-14 11:33:09 +0800

    新建机票搜索组件和搜索建议分析

---
+++ components/air/searchForm.vue
@@ -0,0 +1,246 @@
+<template>
+    <div class="search-form">
+
+        <!-- 头部tab切换 -->
+        <el-row type="flex" class="search-tab">
+            <span 
+            v-for="(item, index) in tabs" 
+            :key="index"
+            @click="handleSearchTab(item, index)"
+            :class="{active: index === currentTab}">
+                <i :class="item.icon"></i>{{item.name}}
+            </span>
+        </el-row>
+
+        <el-form class="search-form-content" ref="form" label-width="80px">
+
+            <el-form-item label="出发城市">
+                
+                <!-- fetch-suggestions: 类似于input方法，每次输入框值发生变化时候回触发 -->
+                <!-- select：选中下拉列表中的值的时候触发的触发  -->
+                <el-autocomplete
+                :fetch-suggestions="queryDepartSearch"
+                placeholder="请搜索出发城市"
+                @select="handleDepartSelect"
+                class="el-autocomplete"
+                v-model="form.departCity"
+                ></el-autocomplete>
+            </el-form-item>
+
+            
+            <el-form-item label="到达城市">
+                <el-autocomplete
+                :fetch-suggestions="queryDestSearch"
+                placeholder="请搜索到达城市"
+                @select="handleDestSelect"
+                class="el-autocomplete"
+                ></el-autocomplete>
+            </el-form-item>
+
+            <el-form-item label="出发时间">
+                <!-- change 用户确认选择日期时触发 -->
+                <el-date-picker type="date" 
+                placeholder="请选择日期" 
+                style="width: 100%;"
+                @change="handleDate">
+                </el-date-picker>
+            </el-form-item>
+
+            <el-form-item label="">
+                <el-button style="width:100%;" 
+                type="primary" 
+                icon="el-icon-search"
+                @click="handleSubmit">
+                    搜索
+                </el-button>
+            </el-form-item>
+            <div class="reverse">
+                <span @click="handleReverse">换</span>
+            </div>
+        </el-form>  
+      </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            tabs: [
+                {icon: "iconfont icondancheng", name: "单程"},
+                {icon: "iconfont iconshuangxiang", name: "往返"}
+            ],
+            currentTab: 0,
+
+            // 最终表单要提交的属性
+            form: {
+                departCity: "", // 出发城市
+                departCode: "", // 出发城市代码
+                destCity: "",  // 到达城市
+                destCode: "",  // 到达城市代码
+                departDate: "", // 日期字符串
+            }
+        }
+    },
+    methods: {
+        // tab切换时触发
+        handleSearchTab(item, index){
+            
+        },
+        
+        // 出发城市输入框值发生变化时候会触发
+        // value：输入框的值
+        // cb:回调函数，必须要调用，调用时候必须要传递一个数组的参数，
+        // 数组中的元素必须是一个对象，对象中必须要有value属性
+        queryDepartSearch(value, cb){
+            
+            // 请求搜索建议城市
+            
+
+            // var arr = [
+            //     {value: "广州"},
+            //     {value: "广元"},
+            // ]
+
+            cb(arr)
+        },
+
+        // 目标城市输入框获得焦点时触发
+        // value 是选中的值，cb是回调函数，接收要展示的列表
+        queryDestSearch(value, cb){
+            
+        },
+       
+        // 出发城市下拉选择时触发
+        handleDepartSelect(item) {
+            
+        },
+
+        // 目标城市下拉选择时触发
+        handleDestSelect(item) {
+            
+        },
+
+        // 确认选择日期时触发
+        handleDate(value){
+           
+        },
+
+        // 触发和目标城市切换时触发
+        handleReverse(){
+            
+        },
+
+        // 提交表单是触发
+        handleSubmit(){
+           
+        }
+    },
+    mounted() {
+       
+    }
+}
+</script>
+
+<style scoped lang="less">
+.search-form{
+    border:1px #ddd solid;
+    border-top:none;
+    width:360px;
+    height:350px;
+    box-sizing: border-box;
+}
+
+.search-tab{
+  span{
+    display: block;
+    flex:1;
+    text-align: center;
+    height:48px;
+    line-height: 42px;
+    box-sizing: border-box;
+    border-top:3px #eee solid;
+    background:#eee;
+  }
+
+  .active{
+    border-top-color: orange;
+    background:#fff;
+  }
+
+  i{
+    margin-right:5px;
+    font-size: 18px;
+
+    &:first-child{
+      font-size:16px;
+    }
+  }
+}
+
+.search-form-content{
+  padding:15px 50px 15px 15px;
+  position: relative;
+
+  .el-autocomplete{
+    width: 100%;
+  }
+}
+
+.reverse{
+  position:absolute;
+  top: 35px;
+  right:15px;
+
+  &:after,&:before{
+      display: block;
+      content: "";
+      position: absolute;
+      left:-35px;
+      width:25px;
+      height:1px;
+      background:#ccc;
+  }
+
+  &:after{
+      top:0;
+    }
+
+    &:before{
+      top:60px;
+    }
+
+  span{
+    display: block;
+    position:absolute;
+    top: 20px;
+    right:0;
+    font-size:12px;
+    background: #999;
+    color:#fff;
+    width:20px;
+    height:20px;
+    line-height: 18px;
+    text-align: center;
+    border-radius: 2px;
+    cursor: pointer;
+
+    &:after,&:before{
+      display: block;
+      content: "";
+      position: absolute;
+      left:10px;
+      width:1px;
+      height:20px;
+      background:#ccc;
+    }
+
+    &:after{
+      top:-20px;
+    }
+
+    &:before{
+      top:20px;
+    }
+  }
+}
+</style>
--- pages/air/index.vue
+++ pages/air/index.vue
@@ -1,13 +1,167 @@
 <template>
-  <div>air</div>
+  <section class="container">
+    <h2 class="air-title"><span class="iconfont iconfeiji"></span>
+      <i>国内机票</i>
+    </h2>
+
+    <!-- 搜索广告栏 -->
+    <el-row type="flex" justify="space-between">
+        <!-- 搜索表单 -->
+        <SearchForm/>
+
+        <!-- banner广告 -->
+        <div class="sale-banner">
+            <img src="http://157.122.54.189:9093/images/pic_sale.jpeg">
+        </div>
+    </el-row>
+
+    <!-- 广告 -->
+    <el-row type="flex" class="statement">
+      <el-col :span="8">
+        <i class="iconfont iconweibiaoti-_huabanfuben" style="color:#409EFF;"></i>
+        <span>100%航协认证</span>
+      </el-col>
+      <el-col :span="8">
+        <i class="iconfont iconbaozheng" style="color:green;"></i>
+        <span>出行保证</span>
+      </el-col>
+      <el-col :span="8">
+        <i class="iconfont icondianhua" style="color:#409EFF;"></i>
+        <span>7x24小时服务</span>
+      </el-col>
+    </el-row>
+
+    <h2 class="air-sale-title">
+      <span class="iconfont icontejiajipiao"></span>
+      <i>特价机票</i>
+    </h2>
+
+    <!-- 特价机票 -->
+    <div class="air-sale">
+        
+    </div>
+  </section>
 </template>
 
 <script>
+import SearchForm from "@/components/air/searchForm"
 export default {
-
+  components: {
+    SearchForm
+  }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.air-sale{
+  border: 1px #ddd solid;
+  padding:20px;
+  margin-bottom:50px;
 
+  .air-sale-pic{
+    > div{
+      width:225px;
+      height:140px;
+      position: relative;
+      overflow: hidden;
+
+      img{
+        width:100%;
+      }
+
+      .layer-bar{
+        position:absolute;
+        bottom:0;
+        left:0;
+        background: rgba(0,0,0,0.5);
+        color:#fff;
+        height:30px;
+        line-height: 30px;
+        width:100%;
+        box-sizing: border-box;
+        padding: 0 15px;
+        font-size: 14px;
+
+        span:last-child{
+          font-size:18px;
+        }
+      }
+    }
+  }
+}
+
+.air-sale-group{
+  margin-top:20px;
+  padding-top:8px;
+  border-right:1px #eee solid;
+
+  &:last-child{
+    border-right:none;
+  }
+
+  .air-sale-row{
+    font-size:12px;
+    color:#666;
+    margin-bottom:8px;
+
+    .air-sale-price{
+      color:orange;
+      font-size: 20px;
+    }
+  }
+}
+
+.container{
+  width:1000px;
+  margin:0 auto;
+}
+
+.air-title{
+  margin:15px 0;
+  font-size:20px;
+  font-weight: normal;
+  color:orange;
+
+  span{
+    font-size:20px;
+  }
+}
+
+.statement{
+  margin:15px 0;
+  border:1px #ddd solid;
+  background:#f5f5f5;
+  height: 58px;
+  padding:10px 0;
+  box-sizing:border-box;
+
+  > div{
+    text-align: center;
+    line-height: 38px;
+    border-right:1px #ddd solid;
+
+    &:last-child{
+      border-right: none;
+    }
+
+    *{
+      vertical-align: middle;
+    }
+
+    i{
+      font-size:30px;
+    }
+  }
+}
+
+.air-sale-title{
+  margin:15px 0;
+  font-size:20px;
+  font-weight: normal;
+  color:#409EFF;
+
+  span{
+    font-size:20px;
+  }
+}
 </style>
