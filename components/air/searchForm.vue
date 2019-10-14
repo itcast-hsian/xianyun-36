5a6000834d48f0f0aaa7aad36e7c44dcae02b2e2
itcast-hsian <541510140@qq.com>
2019-10-14 15:02:48 +0800

    处理到达城市和失去焦点的封装

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -24,7 +24,7 @@
                 @select="handleDepartSelect"
                 class="el-autocomplete"
                 v-model="form.departCity"
-                @blur="handleDepartBlur"
+                @blur="handleBlur(`depart`)"
                 ></el-autocomplete>
             </el-form-item>
 
@@ -35,6 +35,8 @@
                 placeholder="请搜索到达城市"
                 @select="handleDestSelect"
                 class="el-autocomplete"
+                v-model="form.destCity"
+                @blur="handleBlur(`dest`)"
                 ></el-autocomplete>
             </el-form-item>
 
@@ -125,18 +127,32 @@ export default {
         },
 
         // 出发城市失去焦点时候默认选中第一个
-        handleDepartBlur(){
+        // type可能等于depart 或者 dest
+        handleBlur(type){
             // 默认选中城市列表第一个
-            if(this.cities.length > 0){
-                this.form.departCity = this.cities[0].value;
-                this.form.departCode = this.cities[0].sort;
-            }  
-        },
+            // if(this.cities.length > 0){
+            //     if(type === "depart"){
+            //         this.form.departCity = this.cities[0].value;
+            //         this.form.departCode = this.cities[0].sort;
+            //     }
+            //     if(type === "dest"){
+            //         this.form.destCity = this.cities[0].value;
+            //         this.form.destCode = this.cities[0].sort;
+            //     }
+            // }  
 
+            // 另一种写法
+            if(this.cities.length === 0) return;
+
+            this.form[type + "City"] = this.cities[0].value;
+            this.form[type + "Code"] = this.cities[0].sort;
+        },
+ 
         // 目标城市输入框获得焦点时触发
         // value 是选中的值，cb是回调函数，接收要展示的列表
         queryDestSearch(value, cb){
-            
+            // value是到达城市value, cb也是到达的输入框回调函数
+            this.queryDepartSearch(value, cb);
         },
        
         // 出发城市下拉选择时触发
@@ -148,7 +164,9 @@ export default {
 
         // 目标城市下拉选择时触发
         handleDestSelect(item) {
-            
+            // 获取到表单需要的机票信息
+            this.form.destCity = item.value;
+            this.form.destCode = item.sort;
         },
 
         // 确认选择日期时触发
