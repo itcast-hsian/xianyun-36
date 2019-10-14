20fdd17d907f3342b8bebdc7c70e0f6bbe1abfb1
itcast-hsian <541510140@qq.com>
2019-10-14 14:46:25 +0800

    输入框失去焦点时候默认选中第一个

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -24,6 +24,7 @@
                 @select="handleDepartSelect"
                 class="el-autocomplete"
                 v-model="form.departCity"
+                @blur="handleDepartBlur"
                 ></el-autocomplete>
             </el-form-item>
 
@@ -78,7 +79,10 @@ export default {
                 destCity: "",  // 到达城市
                 destCode: "",  // 到达城市代码
                 departDate: "", // 日期字符串
-            }
+            },
+            
+            // 存放newData的城市的数组
+            cities: []
         }
     },
     methods: {
@@ -110,11 +114,23 @@ export default {
                 const newData = data.map(v => {
                     v.value = v.name.replace("市", ""); // 乌鲁市齐市
                     return v;
-                })
+                });
+
+                // 把newData赋值给data中cities
+                this.cities = newData;
 
                 // 展示到下拉列表
                 cb(newData)
             })
+        },
+
+        // 出发城市失去焦点时候默认选中第一个
+        handleDepartBlur(){
+            // 默认选中城市列表第一个
+            if(this.cities.length > 0){
+                this.form.departCity = this.cities[0].value;
+                this.form.departCode = this.cities[0].sort;
+            }  
         },
 
         // 目标城市输入框获得焦点时触发
