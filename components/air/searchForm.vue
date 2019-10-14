d5d85d9a5e659cb4f811d199d399220ef76fc7f9
itcast-hsian <541510140@qq.com>
2019-10-14 15:33:46 +0800

    自定义验证表单

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -41,12 +41,15 @@
             </el-form-item>
 
             <el-form-item label="出发时间">
+
                 <!-- change 用户确认选择日期时触发 -->
                 <el-date-picker type="date" 
                 placeholder="请选择日期" 
                 style="width: 100%;"
-                @change="handleDate">
+                @change="handleDate"
+                v-model="form.departDate">
                 </el-date-picker>
+
             </el-form-item>
 
             <el-form-item label="">
@@ -65,6 +68,8 @@
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
     data(){
         return {
@@ -171,7 +176,7 @@ export default {
 
         // 确认选择日期时触发
         handleDate(value){
-           
+           this.form.departDate = moment(value).format(`YYYY-MM-DD`);
         },
 
         // 触发和目标城市切换时触发
@@ -181,7 +186,43 @@ export default {
 
         // 提交表单是触发
         handleSubmit(){
-            console.info(this.form)
+
+            // 自定义验证
+            const rules = {
+                departCity: {
+                    // message是错误的信息， value是对应表单中的值
+                    message: "请输入出发城市", value: this.form.departCity
+                },
+                destCity: {
+                    message: "请输入到达城市", value: this.form.destCity
+                },
+                departDate: {
+                    message: "请选择出发时间", value: this.form.departDate
+                }
+            }
+
+            // 循环rules这个对象，判断对象属性的value如果是空的，打印出message错误信息
+            let valid = true;
+
+            Object.keys(rules).forEach(v => {
+                // 只要有一次验证不通过，后台验证不用再执行
+                if(!valid) return;
+
+                const {message, value } = rules[v];
+                // 对象属性的value如果是空的
+                if(!value){
+                    this.$message.error(message)
+                    // 验证不通过
+                    valid = false;
+                }
+            })
+
+            if(!valid) return;
+
+            this.$router.push({
+                path: "/air/flights",
+                query: this.form
+            })
         }
     },
     mounted() {
