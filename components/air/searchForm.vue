c3c7e9ab4c975de69bc4ce82f3dcea2c6844623e
itcast-hsian <541510140@qq.com>
2019-10-14 15:54:41 +0800

    城市切换和往返

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -95,7 +95,12 @@ export default {
     methods: {
         // tab切换时触发
         handleSearchTab(item, index){
-            
+            if(index === 1){
+                this.$alert('目前不支持往返', '提示', {
+                    confirmButtonText: '确定',
+                    type: "warning" 
+                });
+            }
         },
         
         // 出发城市输入框值发生变化时候会触发
@@ -181,7 +186,13 @@ export default {
 
         // 触发和目标城市切换时触发
         handleReverse(){
-            
+            const {departCity, departCode, destCity, destCode} = this.form;
+
+            this.form.departCity = destCity;
+            this.form.departCode = destCode;
+
+            this.form.destCity = departCity;
+            this.form.destCode = departCode;
         },
 
         // 提交表单是触发
