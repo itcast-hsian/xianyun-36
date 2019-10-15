f79ab6bba7d2497cc19d87a1c563d77492537343
itcast-hsian <541510140@qq.com>
2019-10-15 11:49:00 +0800

    使用computed实现dataList

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -47,11 +47,10 @@ export default {
     data(){
         return {
             // 请求机票列表返回的总数据，包含了flights,info, options,total
-            flightsData: {},
-
-            // 从flights总列表数据中切割出来数组列表
-            dataList: [],
-
+            flightsData: {
+                // 初始值
+                flights: []
+            },
             // 当前的页数
             pageIndex: 1,
             // 当前的条数
@@ -64,27 +63,28 @@ export default {
         FlightsItem
     },
 
+    computed: {
+        // 从flights总列表数据中切割出来数组列表
+        dataList(){
+            const arr = this.flightsData.flights.slice(
+                (this.pageIndex - 1) * this.pageSize, 
+                this.pageIndex * this.pageSize
+            )
+            return arr;
+        }
+    },
+
     methods: {
         // 分页条数切换时候触发, val是当前的条数
         handleSizeChange(val){
             // 切换条数
             this.pageSize = val;
-            // 重新回到第一页
-            this.dataList = this.flightsData.flights.slice(0, this.pageSize)
         },
 
         // 页数切换时候触发, val是当前的页数
         handleCurrentChange(val){
             // 修改当前的页数
             this.pageIndex = val;
-            // 修改机票列表
-            // 0, 5
-            // 5, 10
-            // 10, 15
-            this.dataList = this.flightsData.flights.slice(
-                (this.pageIndex - 1 ) * this.pageSize,
-                this.pageIndex * this.pageSize
-            )
         }
     },
 
@@ -97,9 +97,6 @@ export default {
         }).then(res => {
             // 保存到机票的总数据
             this.flightsData = res.data;
-
-            // 第一页的数据
-            this.dataList = this.flightsData.flights.slice(0, this.pageSize)
         })
     }
 }
