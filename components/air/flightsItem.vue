7be3bc7680efd15aafa0ae4d56c503bf4a49b42b
itcast-hsian <541510140@qq.com>
2019-10-16 14:45:30 +0800

    跳转到订单详情页

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -47,11 +47,13 @@
                             ￥{{seat.org_settle_price}}
                         </el-col>
                         <el-col :span="3" class="choose-button">
-                            <el-button 
-                            type="warning" 
-                            size="mini">
-                            选定
-                            </el-button>
+                            <nuxt-link :to="`/air/order?id=${item.id}&seat_xid=${seat.seat_xid}`">
+                                <el-button 
+                                type="warning" 
+                                size="mini">
+                                选定
+                                </el-button>
+                            </nuxt-link>
                             <p>剩余：{{seat.discount}}</p>
                         </el-col>
                     </el-row>
