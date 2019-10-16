77a248d8ede3191ac363ba52ff96ce839af41da4
itcast-hsian <541510140@qq.com>
2019-10-16 09:26:29 +0800

    机票列表头部条件过滤的布局

---
+++ components/air/flightsFilters.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="filters">
+        <el-row type="flex" class="filters-top" justify="space-between" align="middle">
+            <!-- 左侧的信息 -->
+            <el-col :span="8">
+                单程： 
+                广州 - 上海 
+                / 
+                2019-06-17
+            </el-col>
+
+            <!-- 过滤条件的列表 -->
+            <!-- option里面的label是显示的界面上 -->
+            <!-- 在下拉选项中选中值的时候取的value值 -->
+            <el-col :span="4">
+                <el-select size="mini" v-model="airport" placeholder="起飞机场" @change="handleAirport">
+                    <el-option
+                    label="abc"
+                    value="123"
+                    >
+                    </el-option>
+                </el-select>
+            </el-col>
+            <el-col :span="4">
+                <el-select size="mini" v-model="flightTimes"  placeholder="起飞时间" @change="handleFlightTimes">
+                    <el-option
+                    label="00:00 - 06:00"
+                    value="1"
+                    >
+                    </el-option>
+                </el-select>
+            </el-col>
+            <el-col :span="4">
+                <el-select size="mini" v-model="company"  placeholder="航空公司" @change="handleCompany">
+                    <el-option
+                    label="厦门航空"
+                    value="厦门航空">
+                    </el-option>
+                </el-select>
+            </el-col>
+            <el-col :span="4">
+                <el-select size="mini" v-model="airSize" placeholder="机型" @change="handleAirSize">
+                    <el-option
+                    label="大"
+                    value="大">
+                    </el-option>
+                </el-select>
+            </el-col>
+        </el-row>
+        <div class="filter-cancel">
+            筛选：
+            <el-button 
+                       type="primary" 
+                       round 
+                       plain 
+                       size="mini" 
+                       @click="handleFiltersCancel">
+                撤销
+    		</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            airport: "",        // 机场
+            flightTimes: "",    // 出发时间
+            company: "",        // 航空公司
+            airSize: "",        // 机型大小
+        }
+    },
+    methods: {
+        // 选择机场时候触发
+        handleAirport(value){
+            console.log(value)
+        },
+
+        // 选择出发时间时候触发
+        handleFlightTimes(value){
+            
+        },
+
+         // 选择航空公司时候触发
+        handleCompany(value){
+            
+        },
+
+         // 选择机型时候触发
+        handleAirSize(value){
+           
+        },
+        
+        // 撤销条件时候触发
+        handleFiltersCancel(){
+            
+        },
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .filters{
+        margin-bottom:20px;
+    }
+
+    .filters-top{
+        > div{
+            /deep/ .el-select{
+                margin-left:10px;
+            }
+        }
+    }
+
+    .filter-cancel{
+        margin-top:10px;
+    }
+</style>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -5,9 +5,7 @@
             <!-- 顶部过滤列表 -->
             <div class="flights-content">
                 <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
+                <FlightsFilters/>
                 
                 <!-- 航班头部布局 -->
                 <FlightsListHead/>
@@ -48,6 +46,7 @@
 
 import FlightsListHead from "@/components/air/flightsListHead"
 import FlightsItem from "@/components/air/flightsItem"
+import FlightsFilters from "@/components/air/flightsFilters"
 
 export default {
     data(){
@@ -68,7 +67,8 @@ export default {
 
     components: {
         FlightsListHead,
-        FlightsItem
+        FlightsItem,
+        FlightsFilters
     },
 
     computed: {
