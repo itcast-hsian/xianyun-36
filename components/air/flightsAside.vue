7759513653dcc7babbdb140906ab0335d6904501
itcast-hsian <541510140@qq.com>
2019-10-16 15:07:53 +0800

    侧边栏历史查询记录

---
+++ components/air/flightsAside.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="aside">
+        <div class="statement">
+            <el-row type="flex" justify="space-between" class="statement-list">
+                <el-col :span="8">
+                    <i class="iconfont iconweibiaoti-_huabanfuben" style="color:#409EFF;"></i>
+                    <span>航协认证</span>
+                </el-col>
+                <el-col :span="8">
+                    <i class="iconfont iconbaozheng" style="color:green;"></i>
+                    <span>出行保证</span>
+                </el-col>
+                <el-col :span="8">
+                    <i class="iconfont icondianhua" style="color:#409EFF;"></i>
+                    <span>7x24</span>
+                </el-col>
+            </el-row>
+            <p class="service-tel">
+                免费客服电话：4006345678转2
+            </p>
+        </div>
+
+        <div class="history">
+            <h5>历史查询</h5>
+
+            <nuxt-link :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
+            v-for="(item, index) in $store.state.air.history"
+            :key="index">
+                <el-row type="flex" 
+                justify="space-between" 
+                align="middle"
+                class="history-item">
+                    <div class="air-info">
+                        <div class="to-from">{{item.departCity}} - {{item.destCity}}</div>
+                        <p>{{item.departDate}}</p>
+                    </div>
+                    <span>选择</span>
+                </el-row>
+            </nuxt-link>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+.statement{
+    border:1px #ddd solid;
+
+    .statement-list{
+        padding: 10px 0;
+        > div{
+            text-align: center;
+            i{
+                display: block;
+                font-size: 40px;
+            }
+            span{
+                font-size:12px;
+            }
+        }
+    }
+    .service-tel{
+        height: 32px;
+        line-height: 32px;
+        padding:0 10px;
+        background: #f6f6f6;
+        font-size: 14px;
+    }
+}
+
+.history{
+    border:1px #ddd solid;
+    padding:10px;
+    margin-top:10px;
+
+    h5{
+        font-size: 16px;
+        font-weight: normal;
+        padding-bottom: 10px;
+        border-bottom:1px #eee solid;
+    }
+
+    .history-item{
+        padding:10px 0;
+        font-size: 14px;
+        border-bottom: 1px #eee solid;
+
+        &:last-child{
+            border:none;
+        }
+
+        .to-from{
+            margin-bottom: 5px;
+        }
+
+        p{
+            font-size: 12px;
+            color:#666;
+        }
+
+        span{
+            color:#fff;
+            display: block;
+            padding:2px 10px;
+            background: orange;
+            border-radius: 4px;
+            font-size:12px;
+            cursor: pointer;
+        }
+    }
+}
+</style>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -37,6 +37,7 @@
             <!-- 侧边栏 -->
             <div class="aside">
                 <!-- 侧边栏组件 -->
+                <FlightsAside/>
             </div>
         </el-row>
     </section>
@@ -47,6 +48,7 @@
 import FlightsListHead from "@/components/air/flightsListHead"
 import FlightsItem from "@/components/air/flightsItem"
 import FlightsFilters from "@/components/air/flightsFilters"
+import FlightsAside from "@/components/air/flightsAside"
 
 export default {
     data(){
@@ -79,7 +81,8 @@ export default {
     components: {
         FlightsListHead,
         FlightsItem,
-        FlightsFilters
+        FlightsFilters,
+        FlightsAside
     },
 
     computed: {
@@ -114,28 +117,42 @@ export default {
         handleCurrentChange(val){
             // 修改当前的页数
             this.pageIndex = val;
+        },
+
+        // 获取机票的列表
+        getList(){
+            // 请求机票列表数据
+            this.$axios({
+                url: "/airs",
+                // params是axios的get的参数
+                params: this.$route.query
+            }).then(res => {
+                // 保存到机票的总数据
+                this.flightsData = res.data;
+
+                // 赋值多一分给缓存的对象,一旦赋值之后不能再被修改
+                this.cacheFlightsData = {...res.data};
+
+                // 请求完毕
+                this.loading = false;
+
+                // 分页总数
+                this.total = this.flightsData.total;
+            })
+        }
+    },
+
+    watch: {
+        // 监听路由
+        $route(){
+            // 请求机票列表数据
+            this.getList()
         }
     },
 
     mounted(){
         // 请求机票列表数据
-        this.$axios({
-            url: "/airs",
-            // params是axios的get的参数
-            params: this.$route.query
-        }).then(res => {
-            // 保存到机票的总数据
-            this.flightsData = res.data;
-
-            // 赋值多一分给缓存的对象,一旦赋值之后不能再被修改
-            this.cacheFlightsData = {...res.data};
-
-            // 请求完毕
-            this.loading = false;
-
-            // 分页总数
-            this.total = this.flightsData.total;
-        })
+        this.getList()
     }
 }
 </script>
