b87672930f03387fd1bbbe2c32d6583335c5fcb2
itcast-hsian <541510140@qq.com>
2019-10-18 15:14:46 +0800

    获取订单添加延时

--- pages/air/pay.vue
+++ pages/air/pay.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <!-- <script src="/qrcode.js"></script> -->
         <div class="main">
             <div class="pay-title">
                 支付总金额 <span class="pay-price">￥ {{ order.price }}</span>
@@ -39,15 +40,18 @@ export default {
         // 订单id
         const {id} = this.$route.query;
 
-        // 请求订单详情
-        this.$axios({
-            url: "/airorders/" + id,
-            headers: {
-                Authorization: `Bearer ${this.$store.state.user.userInfo.token}`
-            }
-        }).then(res => {
-            this.order = res.data;
-        })
+        // 等待本地的插件把本地存储的值赋给store之后再执行请求，才可以拿到token
+        setTimeout(() => {
+            // 请求订单详情
+            this.$axios({
+                url: "/airorders/" + id,
+                headers: {
+                    Authorization: `Bearer ${this.$store.state.user.userInfo.token}`
+                }
+            }).then(res => {
+                this.order = res.data;
+            })
+        }, 10) 
     }
 }
 </script>
