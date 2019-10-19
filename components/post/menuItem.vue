c5028947adb26be5eeed9edd042754d2e8f99284
itcast-hsian <541510140@qq.com>
2019-10-19 11:43:42 +0800

    组件递归

---
+++ components/post/menuItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="digui">
+      <div v-for="(item, index) in data" :key="index">
+          {{ item.name }}
+
+        <!-- 自己调用自己 -->
+        <menuItem :data="item.children" v-if="item.children"/>
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "menuItem",
+    props: ['data']
+}
+</script>
+
+
+<style scoped>
+.digui div{
+  padding-left: 50px;
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,13 +1,66 @@
 <template>
-  <div>post</div>
+  <div style="padding: 50px">
+    <h3>组件的递归</h3>
+
+    <!-- 下面的代码无法知道数据到底有多少级 -->
+    <!-- <div v-for="(item, index) in list" :key="index">
+      {{ item.name }}
+
+        <div v-for="(item, index) in item.children" :key="index">
+          {{ item.name }}
+
+          <div v-for="(item, index) in item.children" :key="index">
+            {{ item.name }}
+          </div>
+        </div>
+    </div> -->
+
+    <!-- 组件所接受的值是数组 -->
+    <MenuItem :data="list"/>
+
+  </div>
 </template>
 
 <script>
-export default {
 
+import MenuItem from "@/components/post/menuItem"
+
+export default {
+  components: {
+    MenuItem
+  },
+
+  data(){
+    return {
+      list: [
+        { 
+          name: "服装", 
+          children: [
+            { name: "男装", children: [
+              { name:"正装" },
+              { name:"休闲装" },
+              { name:"运动装" },
+            ]},
+            { name: "女装", children: [
+              { name:"正装" },
+              { name:"休闲装" },
+              { name:"运动装" },
+              { name:"xxx" }
+            ]},
+            { name: "童装" }
+          ]
+        },
+        {
+          name: "电器", 
+          children: [
+            {name: "电视"},
+            {name: "空调"},
+            {name: "冰箱"}
+          ]
+        }
+      ]
+
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
