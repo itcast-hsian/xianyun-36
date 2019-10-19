557f1759534ad8c6f22ba5038131bcae9c46e346
itcast-hsian <541510140@qq.com>
2019-10-19 11:15:11 +0800

    插槽的使用

---
+++ pages/slot.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="padding: 50px">
+      <!-- 调用按钮组件 -->
+      <SlotButton>
+          <span style="color:red">123</span>
+      </SlotButton>
+    </div>
+</template>
+
+<script>
+
+import SlotButton from "@/components/slotButton"
+
+export default {
+    components: {
+        SlotButton
+    }
+}
+</script>
+
+<style>
+
+</style>
